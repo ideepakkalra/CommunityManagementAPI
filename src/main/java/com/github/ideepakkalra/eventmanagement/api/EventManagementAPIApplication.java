8ea08ead28ff9e818b85ada309ef341a413d58b9
Deepak Kalra <deepak.deepakkalra@gmail.com>
2024-09-29 16:28:09 -0400

    Added Spring Boot Starting File.

---
+++ src/main/java/com/github/ideepakkalra/eventmanagement/api/EventManagementAPIApplication.java
@@ -0,0 +1,11 @@
+package com.github.ideepakkalra.eventmanagement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagementAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementAPIApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/ideepakkalra/eventmanagement/api/EventManagementAPIApplicationTest.java
@@ -0,0 +1,12 @@
+package com.github.ideepakkalra.eventmanagement.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class EventManagementAPIApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}

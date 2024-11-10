5acf584882aefb4bc6a4697779bb441c74009912
Deepak Kalra <deepak.deepakkalra@gmail.com>
2024-11-10 08:25:46 -0500

    Refactoring and test setup

--- src/main/java/com/github/ideepakkalra/eventmanagement/EventManagementApplication.java
+++ src/main/java/com/github/ideepakkalra/eventmanagement/EventManagementApplication.java
@@ -6,6 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventManagementApplication {
     public static void main(String[] args) {
+        // Let's do some ascii art ;)
+        System.out.println();
+        System.out.println("  _   _   _   _   _     _   _   _   _   _   _   _   _   _   _     _   _   _ ");
+        System.out.println(" / \\ / \\ / \\ / \\ / \\   / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\   / \\ / \\ / \\");
+        System.out.println("( E | v | e | n | t ) ( M | a | n | a | g | e | m | e | n | t ) ( A | P | I )");
+        System.out.println(" \\_/ \\_/ \\_/ \\_/ \\_/   \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/   \\_/ \\_/ \\_/");
+        System.out.println();
+        System.out.println("+-+-+-+-+-+-+ +-+-+-+-+-+");
+        System.out.println("|D|E|E|P|A|K| |K|A|L|R|A|");
+        System.out.println("+-+-+-+-+-+-+ +-+-+-+-+-+");
         SpringApplication.run(EventManagementApplication.class, args);
     }
 }
--- src/test/java/com/github/ideepakkalra/eventmanagement/EventManagementApplicationTest.java
+++ src/test/java/com/github/ideepakkalra/eventmanagement/EventManagementApplicationTest.java
@@ -1,12 +1,15 @@
 package com.github.ideepakkalra.eventmanagement;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class EventManagementApplicationTest {
 
     @Test
-    void contextLoads() {
+    void testContextLoads(ApplicationContext context) {
+        Assertions.assertNotNull(context);
     }
 }

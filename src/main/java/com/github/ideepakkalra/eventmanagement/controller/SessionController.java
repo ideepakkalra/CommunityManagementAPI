599c11b677a306732faefd081482705821c5fabb
Deepak Kalra <deepak.deepakkalra@gmail.com>
2025-04-30 17:49:39 -0400

    Added validation for LoginRequest. Updated test case.

--- src/main/java/com/github/ideepakkalra/eventmanagement/controller/SessionController.java
+++ src/main/java/com/github/ideepakkalra/eventmanagement/controller/SessionController.java
@@ -3,6 +3,7 @@ package com.github.ideepakkalra.eventmanagement.controller;
 import com.github.ideepakkalra.eventmanagement.model.LoginRequest;
 import com.github.ideepakkalra.eventmanagement.model.LoginResponse;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SessionController {
 
     @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest, HttpSession httpSession) {
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest, HttpSession httpSession) {
         return null;
     }
 }
--- src/test/java/com/github/ideepakkalra/eventmanagement/controller/SessionControllerTest.java
+++ src/test/java/com/github/ideepakkalra/eventmanagement/controller/SessionControllerTest.java
@@ -1,5 +1,8 @@
 package com.github.ideepakkalra.eventmanagement.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.github.ideepakkalra.eventmanagement.model.LoginRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,16 +25,98 @@ public class SessionControllerTest {
     @Autowired
     private SessionController sessionController;
 
+    private static final ObjectWriter OBJECT_WRITER = new ObjectMapper().writer();
+
     @Test
     public void testContextLoad() throws Exception {
         Assertions.assertThat(sessionController).isNotNull();
     }
 
     @Test
+    public void testPost403Response() throws Exception {
+        // With No Content
+        mockMvc.perform(post("/login")
+                        //.with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isForbidden());
+    }
+
+    @Test
     public void testPost400Response() throws Exception {
+        // With No Content
         mockMvc.perform(post("/login")
                         .with(SecurityMockMvcRequestPostProcessors.csrf())
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        LoginRequest loginRequest = new LoginRequest();
+        // With All Fields Null
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With PhoneNumber Blank
+        loginRequest.setPhoneNumber("");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With PhoneNumber Invalid Characters
+        loginRequest.setPhoneNumber("abcd");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With PhoneNumber Invalid Country Code
+        loginRequest.setPhoneNumber("910000000000");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With PhoneNumber Invalid Length
+        loginRequest.setPhoneNumber("+9100000000000000000");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With PhoneNumber Valid
+        loginRequest.setPhoneNumber("+910000000000");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With Passcode Blank
+        loginRequest.setPasscode("");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With Passcode Invalid Characters
+        loginRequest.setPasscode("abcd");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        // With Passcode Invalid Length
+        loginRequest.setPasscode("123456789");
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        loginRequest.setPasscode("000000");
+        // All Good Now
+        mockMvc.perform(post("/login")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(OBJECT_WRITER.writeValueAsBytes(loginRequest)))
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
     }
 }

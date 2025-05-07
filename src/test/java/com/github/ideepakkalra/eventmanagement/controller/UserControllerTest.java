7aca545aa330811b07c9f81ba31483fb4d425cf2
Deepak Kalra <deepak.deepakkalra@gmail.com>
2025-05-06 21:20:49 -0400

    removed un used imports.

--- src/test/java/com/github/ideepakkalra/eventmanagement/controller/UserControllerTest.java
+++ src/test/java/com/github/ideepakkalra/eventmanagement/controller/UserControllerTest.java
@@ -10,8 +10,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;

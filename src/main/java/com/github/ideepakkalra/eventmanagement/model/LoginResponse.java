d1af621a63fd14617a913013fe0150154d776026
Deepak Kalra <deepak.deepakkalra@gmail.com>
2025-05-21 21:19:09 -0400

    Updated CORS config

--- src/main/java/com/github/ideepakkalra/eventmanagement/model/LoginResponse.java
+++ src/main/java/com/github/ideepakkalra/eventmanagement/model/LoginResponse.java
@@ -2,8 +2,10 @@ package com.github.ideepakkalra.eventmanagement.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
